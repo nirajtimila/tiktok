c8caf9fc72f7aaf64bef83b6327c6c44bc81df34
Niraj Timila <nirajtimila@gmail.com>
2025-04-21 12:52:55 +1000

    Create proxies.js

---
+++ proxies.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { ip: '38.153.152.244', port: '9594', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '86.38.234.176', port: '6630', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '173.211.0.148', port: '6641', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '161.123.152.115', port: '6360', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '216.10.27.159', port: '6837', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '154.36.110.199', port: '6853', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '45.151.162.198', port: '6600', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '185.199.229.156', port: '7492', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '185.199.228.220', port: '7300', username: 'rusjbvpq', password: 'rpdc7j7gtr64' },
+  { ip: '185.199.231.45', port: '8382', username: 'rusjbvpq', password: 'rpdc7j7gtr64' }
+];
